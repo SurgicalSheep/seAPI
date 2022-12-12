d4e19a92960bbb7e1d7ac1d39ee8a2859420ab78
SurgicalSheep <43825220+SurgicalSheep@users.noreply.github.com>
2022-12-12 17:40:48 +0000

    Update sensores.js

--- controllers/sensores.js
+++ controllers/sensores.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-let ledQuarto = false;
-let ledSala = false;
-let ledOutside = false;
-let portas = false;
-let ventoinha = false;
-let ledAquecer = false;
-let janela = false;
-let temperatura = 0;
-let co2 = 0;
-let humidity = 0;
+var ledQuarto = false;
+var ledSala = false;
+var ledOutside = false;
+var portas = false;
+var ventoinha = false;
+var ledAquecer = false;
+var janela = false;
+var temperatura = 0;
+var co2 = 0;
+var humidity = 0;
 
 router.get("/estado", async (req, res, next) => {
     res.send({data:[{ledQuarto}, {ledSala}, {ledOutside}, {portas}, {ventoinha}, {ledAquecer}, {janela}]})
@@ -21,10 +21,13 @@ router.get("/dados", async (req, res, next) => {
 
 router.put("/dados", async (req, res, next) => {
     try {
-        let {temperatura,co2,humidity} = req.body;
-    temperatura = temperatura;
-    co2 = co2;
-    humidity = humidity;
+        let {Temperatura,Co2,Humidity} = req.body;
+        if(Temperatura)
+    temperatura = Temperatura;
+    if(Co2)
+    co2 = Co2;
+    if(Humidity)
+    humidity = Humidity;
     res.send({data:"Dados atualizados com sucesso!"})
     } catch (error) {
         next(error);
@@ -34,14 +37,21 @@ router.put("/dados", async (req, res, next) => {
 
 router.put("/estado", async (req, res, next) => {
     try {
-        let {ledQuarto, ledSala, ledOutside, portas, ventoinha, ledAquecer, janela} = req.body;
-        ledQuarto = ledQuarto;
-        ledSala = ledSala;
-        ledOutside = ledOutside;
-        portas = portas;
-        ventoinha = ventoinha;
-        ledAquecer = ledAquecer;
-        janela = janela;
+        let {LedQuarto, LedSala, LedOutside, Portas, Ventoinha, LedAquecer, Janela} = req.body;
+        if(LedQuarto)
+        ledQuarto = LedQuarto;
+        if(LedSala)
+        ledSala = LedSala;
+        if(LedOutside)
+        ledOutside = LedOutside;
+        if(Portas)
+        portas = Portas;
+        if(Ventoinha)
+        ventoinha = Ventoinha;
+        if(LedAquecer)
+        ledAquecer = LedAquecer;
+        if(Janela)
+        janela = Janela;
         res.send({data:"Estado atualizado com sucesso!"})
     } catch (error) {
         next(error);
